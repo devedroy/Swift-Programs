aae70b6d157410e0a98f782133fb2aace200f81d
Deved Roy <44873540+devedroy@users.noreply.github.com>
2021-09-30 19:23:17 +0530

    Indentation
    updated Spacing.

--- CA-1/selectionSort.swift
+++ CA-1/selectionSort.swift
@@ -2,21 +2,27 @@ print("Total number of Inputs?")
 var inpt = readLine()
 var n = ((Int)(inpt!))!
 var array:[Int] = []
-for i in 0..<n{
-let inp = readLine()
-array.insert(((Int)(inp!))!,at:i)
+
+for i in 0..<n {
+  let inp = readLine()
+  array.insert(((Int)(inp!))!,at:i)
 }
-for i in 0..<n{
-var min_idx = i
-for j in i+1..<n{
-if array[min_idx] > array[j]{
-min_idx = j
+
+for i in 0..<n {
+  var min_idx = i
+  for j in i+1..<n{
+    if array[min_idx] > array[j]{
+      min_idx = j
+    }
+  }
+  
+  //swap
+  let temp = array[i] + array[min_idx]
+  array[i] = temp - array[i]
+  array[min_idx] = temp - array[i]
 }
-}//swap
-let temp = array[i] + array[min_idx]
-array[i] = temp - array[i]
-array[min_idx] = temp - array[i]
-}
+
 print("Selection sorted array:")
-for i in 0..<n{
-print(array[i])
+for i in 0..<n {
+  print(array[i])
+}
