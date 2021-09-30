c6ec8f421ed9a58dea2550f9c332cc1d6117e762
Deved Roy <44873540+devedroy@users.noreply.github.com>
2021-09-30 19:17:54 +0530

    Indentation
    spacing

--- CA-1/diffUnionFormUnion.swift
+++ CA-1/diffUnionFormUnion.swift
@@ -1,9 +1,10 @@
-//Returns a new set with the elements of both this set and the given
-sequence .
+//Union
+//Returns a new set with the elements of both this set and the given sequence .
 let attendees: Set = ["Alicia", "Bethany", "Diana"]
 let visitors = ["Marcia", "Nathaniel"]
 let attendeesAndVisitors = attendees.union(visitors)
 print(attendeesAndVisitors)
+
 //formUnion
 //Inserts the elements of the given sequence into the set.
 var attendees: Set = ["Alicia", "Bethany", "Diana"]
